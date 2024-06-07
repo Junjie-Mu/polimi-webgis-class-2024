1d3bbf582eeaf7eed752caad965850ce3d7e339d
Junjie-Mu <junjie.mu@mail.polimi.it>
2024-06-07 23:04:34 +0200

    Homepage, workflow, results

---
+++ assets/js/map.js
@@ -0,0 +1,126 @@
+//OpenStreetMap
+let osm = new ol.layer.Tile({
+    title: "OpenStreetMap",
+    type: 'base',
+    visible: true,
+    source: new ol.source.OSM()
+});
+
+//Create the layer groups and add the layers to them
+let basemapLayers = new ol.layer.Group({
+    title: "Base Maps",
+    layers: [osm]
+});
+let overlayLayers = new ol.layer.Group({
+    title: "Overlay Layers",
+})
+
+// Map Initialization
+let map = new ol.Map({
+    target: document.getElementById('map'),
+    layers: [basemapLayers, overlayLayers],
+    view: new ol.View({
+        center: ol.proj.fromLonLat([9.38773, 45.95328]),
+        zoom: 11
+    })
+});
+
+// Add the map controls:
+map.addControl(new ol.control.ScaleLine()); //Controls can be added using the addControl() map function
+map.addControl(new ol.control.FullScreen());
+map.addControl(
+    new ol.control.MousePosition({
+        coordinateFormat: ol.coordinate.createStringXY(4),
+        projection: 'EPSG:4326',
+        className: 'custom-control',
+        placeholder: '0.0000, 0.0000'
+    })
+);
+
+// Add the layer switch
+var layerSwitcher = new ol.control.LayerSwitcher({});
+map.addControl(layerSwitcher);
+
+//Add the Bing Maps layers
+var BING_MAPS_KEY = "Ajrza5Uk8_QGVZMe5ReBVmyutydBHz1WSIr-2DeCoOYPaAkr1Y8HdB9DXUSfyNpe";
+var bingRoads = new ol.layer.Tile({
+    title: 'Bing Maps—Roads',
+    type: 'base',
+    visible: false,
+    source: new ol.source.BingMaps({
+        key: BING_MAPS_KEY,
+        imagerySet: 'Road'
+    })
+});
+var bingAerial = new ol.layer.Tile({
+    title: 'Bing Maps—Aerial',
+    type: 'base',
+    visible: false,
+    source: new ol.source.BingMaps({
+        key: BING_MAPS_KEY,
+        imagerySet: 'Aerial'
+    })
+});
+basemapLayers.getLayers().extend([bingRoads, bingAerial]);
+
+//Add the code for the Pop-up
+var container = document.getElementById('popup');
+var content = document.getElementById('popup-content');
+var closer = document.getElementById('popup-closer');
+
+var popup = new ol.Overlay({
+    element: container
+});
+map.addOverlay(popup);
+
+// This ensures that JQuery ($) is already available in the page.
+$(document).ready(function () {
+    map.on('singleclick', function (event) {
+        //This iterates over all the features that are located on the pixel of the click (can be many)
+        var feature = map.forEachFeatureAtPixel(event.pixel, function (feature, layer) {
+            return feature;
+        });
+
+        //If there is a feature, open the popup by setting a position to it and put the data from the feature
+        if (feature != null) {
+            var pixel = event.pixel;
+            var coord = map.getCoordinateFromPixel(pixel);
+            popup.setPosition(coord);
+            content.innerHTML =
+                '<h5>Colombia Water Areas</h5><br><b>Name: </b>' +
+                feature.get('NAME') +
+                '</br><b>Description: </b>' +
+                feature.get('HYC_DESCRI');
+        } else {
+            var viewResolution = (map.getView().getResolution());
+            var url = colombiaRoads.getSource().getFeatureInfoUrl(event.coordinate, viewResolution, 'EPSG:3857', { 'INFO_FORMAT': 'text/html' });
+
+            if (url) {
+                var pixel = event.pixel;
+                var coord = map.getCoordinateFromPixel(pixel);
+                popup.setPosition(coord);
+                //We do again the AJAX request to get the data from the GetFeatureInfo request
+                $.ajax({ url: url })
+                    .done((data) => {
+                        //Put the data of the GetFeatureInfo response inside the pop-up
+                        //The data that arrives is in HTML
+                        content.innerHTML = data;
+                    });
+            }
+        }
+    });
+});
+
+// The click event handler for closing the popup.
+closer.onclick = function () {
+    popup.setPosition(undefined);
+    closer.blur();
+    return false;
+};
+
+// Adding map event for pointermove
+map.on('pointermove', function (event) {
+    var pixel = map.getEventPixel(event.originalEvent);
+    var hit = map.hasFeatureAtPixel(pixel);
+    map.getTarget().style.cursor = hit ? 'pointer' : '';
+});
